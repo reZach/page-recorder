0ce00a666db822ac31a55123bb5f54860dbb6f7b
Zachary Wenta <reZach@users.noreply.github.com>
2019-08-03 22:33:05 -0500

    Not using the response

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     let fireAction = function(actionName, callback){
         return chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, { action: actionName }, function (response) {
-                callback(response);
+                callback();
             });
         });
     }
